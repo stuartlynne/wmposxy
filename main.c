e525d615d18587decd56dc272e40abd954b04c9d
stuart lynne <stuart.lynne@gmail.com>
2025-09-26 19:22:39 -0700

    1200x600 for usage example

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ static void usage(const char *prog)
             "requested coordinates. The program must include a --geometry WxH+X+Y token.\n\n"
             "Usage: %s <program> [args...]\n\n"
             "Example:\n"
-            "  %s konsole --geometry 124x48+0+840 -- ssh user@host\n",
+            "  %s konsole --geometry 1200x600+0+840 -- ssh user@host\n",
             prog, prog);
 }
 
